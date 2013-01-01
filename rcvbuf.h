e2d8263c083a552702fe41fee2d466585bf746a6
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2013-01-02 05:00:09 +0900

    Avoid wrong optimization.
    - add explicit volatile keyword that indicates these pointers could be updated
      in interrupt context otherwise RBUF_EMPTY() is checked only once
    - use do {} while (0) for multiple line macro

--- rcvbuf.h
+++ rcvbuf.h
@@ -76,19 +76,25 @@
 #define	RBUF_SIZE	128
 
 struct rcvbuf {
-	u_char	*rb_push;
-	u_char	*rb_pop;
+	u_char	* volatile rb_push;
+	u_char	* volatile rb_pop;
 	u_char	 rb_buf[RBUF_SIZE+4];
 };
 
 #define RBUF_INIT(n)	rcvbuf[n].rb_pop = rcvbuf[n].rb_push = &rcvbuf[n].rb_buf[RBUF_SIZE]
 
-#define PUSH_RBUF(n, c)	*(--rcvbuf[n].rb_push) = c ; \
-			if (rcvbuf[n].rb_push == rcvbuf[n].rb_buf) \
-				rcvbuf[n].rb_push = &rcvbuf[n].rb_buf[RBUF_SIZE]
+#define PUSH_RBUF(n, c)							\
+do {									\
+	*(--rcvbuf[n].rb_push) = c ;					\
+	if (rcvbuf[n].rb_push == rcvbuf[n].rb_buf)			\
+		rcvbuf[n].rb_push = &rcvbuf[n].rb_buf[RBUF_SIZE];	\
+} while (/* CONSTCOND */0)
 
-#define POP_RBUF(n, c)	c= *(--rcvbuf[n].rb_pop); \
-			if (rcvbuf[n].rb_pop == rcvbuf[n].rb_buf) \
-				rcvbuf[n].rb_pop = &rcvbuf[n].rb_buf[RBUF_SIZE]
+#define POP_RBUF(n, c)							\
+do {									\
+	c= *(--rcvbuf[n].rb_pop);					\
+	if (rcvbuf[n].rb_pop == rcvbuf[n].rb_buf)			\
+		rcvbuf[n].rb_pop = &rcvbuf[n].rb_buf[RBUF_SIZE];	\
+} while (0)
 
 #define RBUF_EMPTY(n)	(rcvbuf[n].rb_push == rcvbuf[n].rb_pop ? 1: 0)
