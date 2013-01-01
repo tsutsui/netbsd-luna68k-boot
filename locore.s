bf674efcd8a41e09d9b82b81cb6ecc40f76e62dd
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2013-01-01 21:02:25 +0900

    Use ANSI strings concatination (##).

--- locore.s
+++ locore.s
@@ -630,20 +630,20 @@ rei:					| dummy Entry of rei
 #ifdef GPROF
 #ifdef __GNUC__
 #define	ENTRY(name) \
-	.globl _/**/name; _/**/name: link a6,#0; jbsr mcount; unlk a6
+	.globl _##name; _##name: link a6,#0; jbsr mcount; unlk a6
 #define ALTENTRY(name, rname) \
 	ENTRY(name); jra rname+12
 #else
 #define	ENTRY(name) \
-	.globl _/**/name; _/**/name: jbsr mcount
+	.globl _##name; _##name: jbsr mcount
 #define ALTENTRY(name, rname) \
 	ENTRY(name); jra rname+6
 #endif
 #else
 #define	ENTRY(name) \
-	.globl _/**/name; _/**/name:
+	.globl _##name; _##name:
 #define ALTENTRY(name, rname) \
-	.globl _/**/name; _/**/name:
+	.globl _##name; _##name:
 #endif
 
 /*
