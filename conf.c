cb6813d984f11b668899ab50f16193dc11a77179
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2013-01-01 23:42:43 +0900

    Add conf.c for NetBSD's libsa loader. Taken from hp300.

--- conf.c
+++ conf.c
@@ -1,45 +1,8 @@
+/*	$NetBSD$	*/
+
 /*
- * Copyright (c) 1992 OMRON Corporation.
- *
- * This code is derived from software contributed to Berkeley by
- * OMRON Corporation.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)conf.c	8.1 (Berkeley) 6/10/93
- */
-/*
- * Copyright (c) 1992, 1993
+ * Copyright (c) 1982, 1986, 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * OMRON Corporation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -68,123 +31,86 @@
  *	@(#)conf.c	8.1 (Berkeley) 6/10/93
  */
 
-#include "saio.h"
+#include <sys/param.h>
+#include <sys/socket.h>
 
-int
-devread(struct iob *io)
-{
-	int cc;
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
 
-	io->i_flgs |= F_RDDATA;
-	io->i_error = 0;
-	cc = (*devsw[io->i_dev].dv_strategy)(io, READ);
-	io->i_flgs &= ~F_TYPEMASK;
-	return (cc);
-}
+#include <lib/libsa/stand.h>
+#include <lib/libsa/nfs.h>
+#include <lib/libsa/ufs.h>
 
-int
-devwrite(struct iob *io)
-{
-	int cc;
+#include <luna68k/stand/boot/samachdep.h>
 
-	io->i_flgs |= F_WRDATA;
-	io->i_error = 0;
-	cc = (*devsw[io->i_dev].dv_strategy)(io, WRITE);
-	io->i_flgs &= ~F_TYPEMASK;
-	return (cc);
-}
+#define xxstrategy	\
+	(int (*)(void *, int, daddr_t, size_t, void *, size_t *))nullsys
+#define xxopen		(int (*)(struct open_file *, ...))nodev
+#define xxclose		(int (*)(struct open_file *))nullsys
 
-void
-devopen(struct iob *io)
-{
+/*
+ * Device configuration
+ */
+#ifndef SUPPORT_ETHERNET
+#define	netstrategy	xxstrategy
+#define	netopen		xxopen
+#define	netclose	xxclose
+#endif
+#define	netioctl	noioctl
 
-	(*devsw[io->i_dev].dv_open)(io);
-}
+#ifndef SUPPORT_DISK
+#define	sdstrategy	xxstrategy
+#define	sdopen		xxopen
+#define	sdclose		xxclose
+#endif
+#define	sdioctl		noioctl
 
-void
-devclose(struct iob *io)
-{
-
-	(*devsw[io->i_dev].dv_close)(io);
-}
-
-int
-devioctl(struct iob *io, int cmd, void *arg)
-{
-
-	return ((*devsw[io->i_dev].dv_ioctl)(io, cmd, arg));
-}
-
-/*ARGSUSED*/
-void
-nullsys(struct iob *io)
-{
-
-	;
-}
-
-/*ARGSUSED*/
-int
-nullioctl(struct iob *io, int cmd, void *arg)
-{
-
-	return (ECMD);
-}
-
-int	nullsys(struct iob *), nullioctl(struct iob *, int, void *);
-int	sdstrategy(struct iob *, int), sdopen(struct iob *), sdioctl(dev_t, u_long data[]);
-
+/*
+ * Note: "le" isn't a major offset.
+ */
 struct devsw devsw[] = {
-	{ "sd",	sdstrategy,	sdopen,		nullsys,	nullioctl },
-	{ 0, 0, 0, 0, 0 },
+//	{ "sd",	sdstrategy,	sdopen,	sdclose,	sdioctl },
+//	{ "le",	netstrategy,	netopen, netclose,	netioctl },
 };
+int	ndevs = __arraycount(devsw);
 
-dev_t
-make_device(char *str)
+#ifdef SUPPORT_ETHERNET
+struct netif_driver *netif_drivers[] = {
+	&le_driver,
+};
+int	n_netif_drivers = __arraycount(netif_drivers);
+#endif
+
+#if 0
+/*
+ * Physical unit/lun detection.
+ */
+static int punitzero(int, int, int *);
+
+static int
+punitzero(int ctlr, int slave, int *punit)
 {
-	char *cp;
-	struct devsw *dp;
-	int major, unit, part;
 
-	/*
-	 * parse path strings
-	 */
-							/* find end of dev type name */
-	for (cp = str; *cp && *cp != '('; cp++)
-			;
-	if (*cp != '(') {
-		return (-1);
-	}
-							/* compare dev type name */
-	*cp = '\0';
-	for (dp = devsw; dp->dv_name; dp++)
-		if (!strcmp(str, dp->dv_name))
-			break;
-	*cp++ = '(';
-	if (dp->dv_name == NULL) {
-		return (-1);
-	}
-	major = dp - devsw;
-							/* get unit number */
-	unit = *cp++ - '0';
-	if (*cp >= '0' && *cp <= '9')
-		unit = unit * 10 + *cp++ - '0';
-	if (unit < 0 || unit > 63) {
-		return (-1);
-	}
-							/* get partition offset */
-	if (*cp++ != ',') {
-		return (-1);
-	}
-	part = *cp - '0';
-							/* check out end of dev spec */
-	for (;;) {
-		if (*cp == ')')
-			break;
-		if (*cp++)
-			continue;
-		return (-1);
-	}
-
-	return(major << 8 | unit << 3 | part);
+	*punit = 0;
+	return 0;
 }
+
+#define	sdpunit		punitzero
+#define	lepunit		punitzero
+
+struct punitsw punitsw[] = {
+	{ sdpunit },
+	{ lepunit },
+};
+int	npunit = __arraycount(punitsw);
+#endif
+
+/*
+ * Filesystem configuration
+ */
+struct fs_ops file_system_ufs[] = { FS_OPS(ufs) };
+//struct fs_ops file_system_nfs[] = { FS_OPS(nfs) };
+
+struct fs_ops file_system[1];
+int	nfsys = 1;		/* we always know which one we want */
