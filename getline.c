9afbb39ea5885face2ac2d1892d677972868e249
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2014-01-03 15:32:11 +0900

    Pull a fix from OpenBSD/luna88k:
    - Bring getline() - a.k.a libsa gets() with a prompt prefix - in par with libsa
      gets(), featurewise; this means support for ^u to clear the input.

--- getline.c
+++ getline.c
@@ -82,37 +82,51 @@ int
 getline(const char *prompt, char *buff)
 {
 	int c;
-	char *p = buff;
+	char *p, *lp = buff;
 
 	printf("%s", prompt);
 
-	for(;;) {
-		c = getchar() & 0x7F;
+	for (;;) {
+		c = getchar() & 0x7f;
 
 		switch (c) {
-		case 0x0a:
-		case 0x0d:
+		case '\n':
+		case '\r':
+			*lp = '\0';
 			putchar('\n');
-			*p = '\0';
 			goto outloop;
 
-		case 0x08:
+		case '\b':
 		case 0x7f:
-			if (p > buff) {
-				putchar(0x08);
+			if (lp > buff) {
+				lp--;
+				putchar('\b');
 				putchar(' ');
-				putchar(0x08);
-				p--;
+				putchar('\b');
 			}
 			break;
 
+		case 'r' & 0x1f:
+			putchar('\n');
+			printf("%s", prompt);
+			for (p = buff; p < lp; ++p)
+				putchar(*p);
+			break;
+
+		case 'u' & 0x1f:
+		case 'w' & 0x1f:
+			lp = buff;
+			printf("\n%s", prompt);
+			break;
+
 		default:
-			*p++ = c;
+			*lp++ = c;
 			putchar(c);
 			break;
 		}
 	}
 
  outloop:
-	return(strlen(buff));
+	*lp = '\0';
+	return lp - buff;
 }
